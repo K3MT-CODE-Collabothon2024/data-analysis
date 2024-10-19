84d9fff294f0954759ff0e579682f339d1fa7602
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-10-19 15:57:21 +0200

    Enable cors

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 from services.yfinance import fetch_stock_data, fetch_currency_data
 from services.account_summary import get_customer_assets_from_db
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/api/stock/<ticker>', methods=['GET'])
 def get_stock_data(ticker):
