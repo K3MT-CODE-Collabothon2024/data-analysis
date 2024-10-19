fae46b65dec7ca47a760ae20ec999e072e4f4de2
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-10-19 18:50:55 +0200

    Added current and historical data fetching

--- main.py
+++ main.py
@@ -1,31 +1,31 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 
-from services.yfinance import fetch_stock_data, fetch_currency_data
+from services.yfinance import fetch_current_currency_data, fetch_current_stock_data, fetch_historical_stock_data, fetch_historical_currency_data
 from services.account_summary import get_customer_assets_from_db
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/api/stock/<ticker>', methods=['GET'])
+@app.route('/api/stock/historical/<ticker>', methods=['GET'])
 def get_historical_stock_data(ticker):
     try:
         interval_param = request.args.get('interval', '1mo')
         period_param = request.args.get('period', '1y')
 
-        data = fetch_stock_data(ticker, interval_param, period_param)
+        data = fetch_historical_stock_data(ticker, interval_param, period_param)
         return jsonify(data), 200
 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-@app.route('/api/currency/<ticker>', methods=['GET'])
+@app.route('/api/currency/historical/<ticker>', methods=['GET'])
 def get_historical_currency_data(ticker):
     try:
         interval_param = request.args.get('interval', '1mo')
         period_param = request.args.get('period', '1y')
 
-        data = fetch_currency_data(ticker, interval_param, period_param)
+        data = fetch_historical_currency_data(ticker, interval_param, period_param)
         return jsonify(data), 200
 
     except Exception as e:
@@ -34,10 +34,7 @@ def get_historical_currency_data(ticker):
 @app.route('/api/stock/<ticker>', methods=['GET'])
 def get_current_stock_data(ticker):
     try:
-        interval_param = request.args.get('interval', '1mo')
-        period_param = request.args.get('period', '1y')
-
-        data = fetch_stock_data(ticker, interval_param, period_param)
+        data = fetch_current_stock_data(ticker)
         return jsonify(data), 200
 
     except Exception as e:
@@ -46,10 +43,7 @@ def get_current_stock_data(ticker):
 @app.route('/api/currency/<ticker>', methods=['GET'])
 def get_current_currency_data(ticker):
     try:
-        interval_param = request.args.get('interval', '1mo')
-        period_param = request.args.get('period', '1y')
-
-        data = fetch_currency_data(ticker, interval_param, period_param)
+        data = fetch_current_currency_data(ticker)
         return jsonify(data), 200
 
     except Exception as e:
@@ -63,6 +57,8 @@ def get_customer_assets(customer_id):
 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- services/yfinance.py
+++ services/yfinance.py
@@ -1,6 +1,6 @@
 import yfinance as yf
 
-def fetch_stock_data(ticker, interval, period):
+def fetch_historical_stock_data(ticker, interval, period):
     try:
         stock = yf.Ticker(ticker)
         stock_info = stock.history(interval=interval, period=period)
@@ -22,13 +22,57 @@ def fetch_stock_data(ticker, interval, period):
     except Exception as e:
         return {"error": str(e)}
 
-def fetch_currency_data(currency_pair, interval, period):
+def fetch_historical_currency_data(currency_pair, interval, period):
     try:
         currency = yf.Ticker(currency_pair)
         currency_info = currency.history(interval=interval, period=period)
 
         data = []
         for index, row in currency_info.iterrows():
+            data.append({
+                "currency_pair": currency_pair.upper(),
+                "date": index.strftime('%Y-%m-%d'),
+                "open": float(row['Open']),
+                "close": float(row['Close']),
+                "high": float(row['High']),
+                "low": float(row['Low']),
+                "volume": int(row['Volume']),
+            })
+
+        return data
+
+    except Exception as e:
+        return {"error": str(e)}
+
+def fetch_current_stock_data(ticker):
+    try:
+        stock = yf.Ticker(ticker)
+        stock_info = stock.history(interval='1m')
+
+        data = []
+        for index, row in stock_info.iterrows():
+            data.append({
+                "ticker": ticker.upper(),
+                "date": index.strftime('%Y-%m-%d'),
+                "open": float(row['Open']),
+                "close": float(row['Close']),
+                "high": float(row['High']),
+                "low": float(row['Low']),
+                "volume": int(row['Volume']),
+            })
+
+        return data
+
+    except Exception as e:
+        return {"error": str(e)}
+
+def fetch_current_currency_data(currency_pair):
+    try:
+        currency = yf.Ticker(currency_pair)
+        stock_info = currency.history(interval='1m')
+
+        data = []
+        for index, row in stock_info.iterrows():
             data.append({
                 "currency_pair": currency_pair.upper(),
                 "date": index.strftime('%Y-%m-%d'),
