21fd9c3d8f0f2f6d6821e9434e12cc1132d2cb13
Timothy Kalejaiye <tkalejaiye012@gmail.com>
2024-10-19 11:35:21 +0200

    Created api endpoint for fetching currency data from yfinance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, request
 
-from services.yfinance import fetch_stock_data
+from services.yfinance import fetch_stock_data, fetch_currency_data
 
 app = Flask(__name__)
 
@@ -11,6 +11,18 @@ def get_stock_data(ticker):
         period_param = request.args.get('period', '1y')
 
         data = fetch_stock_data(ticker, interval_param, period_param)
+        return jsonify(data), 200
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+@app.route('/api/currency/<ticker>', methods=['GET'])
+def get_currency_data(ticker):
+    try:
+        interval_param = request.args.get('interval', '1mo')
+        period_param = request.args.get('period', '1y')
+
+        data = fetch_currency_data(ticker, interval_param, period_param)
         return jsonify(data), 200
 
     except Exception as e:
--- services/yfinance.py
+++ services/yfinance.py
@@ -21,3 +21,27 @@ def fetch_stock_data(ticker, interval, period):
 
     except Exception as e:
         return {"error": str(e)}
+
+import yfinance as yf
+
+def fetch_currency_data(currency_pair, interval, period):
+    try:
+        currency = yf.Ticker(currency_pair)
+        currency_info = currency.history(interval=interval, period=period)
+
+        data = []
+        for index, row in currency_info.iterrows():
+            data.append({
+                "currency_pair": currency_pair.upper(),
+                "date": index.strftime('%Y-%m-%d'),
+                "open": float(row['Open']),
+                "close": float(row['Close']),
+                "high": float(row['High']),
+                "low": float(row['Low']),
+                "volume": int(row['Volume']),
+            })
+
+        return data
+
+    except Exception as e:
+        return {"error": str(e)}
